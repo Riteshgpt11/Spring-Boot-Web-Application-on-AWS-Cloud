cc003ea792113bea81208753544527cc44c21241
yogitaj508 <jain.yo@husky.neu.edu>
2017-09-28 12:27:28 -0400

    Update Demo.java

--- src/test/java/com/csye6225/demo/rest_assured/Demo.java
+++ src/test/java/com/csye6225/demo/rest_assured/Demo.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
+import org.springframework.context.annotation.Configuration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
