9f56009fd345b712d80394b9d409a87e99a8efca
Riteshgpt11 <gupta.rite@husky.neu.edu>
2017-10-02 20:04:44 -0400

    Added command for sg id

--- script/terminate-ec2-instance.sh
+++ script/terminate-ec2-instance.sh
@@ -3,6 +3,10 @@
 ##Disabling instance termination protection
 aws ec2 modify-instance-attribute --instance-id $1 --no-disable-api-termination
 
+##Fetching Security Group Id
+sg_id=`aws ec2 describe-instances --instance-ids $1 | grep SECURITYGROUPS | awk '{print $2}'`
+echo $sg_id
+
 ##Terminating the instance
 aws ec2 terminate-instances --instance-ids $1
 
@@ -10,6 +14,6 @@ aws ec2 terminate-instances --instance-ids $1
 aws ec2 wait instance-terminated --instance-ids $1
 
 ##Deleting the security group
-aws ec2 delete-security-group --group-id $2
+aws ec2 delete-security-group --group-id "$sg_id"
 
 
