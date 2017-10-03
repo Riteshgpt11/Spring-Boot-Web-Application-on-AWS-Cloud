faccad9fd5280493d90797fae6ac951147e104f6
PratikshaShetty <you@example.com>
2017-10-02 21:52:28 -0400

    Added termination protection in the create stack script and added script to delete stack

--- cloudformation/create-csye6225-cloudformation-stack.sh
+++ cloudformation/create-csye6225-cloudformation-stack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name "$1" --template-body file://create-ec2-stack.yml --parameters file://ec2-parameters.json
+aws cloudformation create-stack  --enable-termination-protection  --stack-name "$1"  --template-body file://create-ec2-stack.yml --parameters file://ec2-parameters.json
 #aws cloudformation update-stack update--termination-protection --stack-name "$1"
--- cloudformation/delete-csye6225-cloudformation-stack.sh
+++ cloudformation/delete-csye6225-cloudformation-stack.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+#Updating the termination protection state from enabled to disabled 
+aws cloudformation update-termination-protection --no-enable-termination-protection --stack-name "$1"
+
+#Deleting the stack
+aws cloudformation delete-stack  --stack-name "$1"
+
+
