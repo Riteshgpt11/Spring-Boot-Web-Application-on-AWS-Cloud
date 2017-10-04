6d48fab3335e7142772b2dc9506a35230e8aed05
Riteshgpt11 <31876546+Riteshgpt11@users.noreply.github.com>
2017-10-04 09:29:23 -0400

    Update delete-csye6225-cloudformation-stack.sh

--- cloudformation/delete-csye6225-cloudformation-stack.sh
+++ cloudformation/delete-csye6225-cloudformation-stack.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Yogita Jain, jain.yo@husky.neu.edu, 001643815
+#Rohan Magare, magare.r@husky.neu.edu, 001231457
+#Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
+#Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
+
 # fetch instance id
 instanceid=`aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filters "Name=tag-key,Values=aws:cloudformation:stack-name" "Name=tag-value,Values=$1" --output text`
 echo "$instanceid"
