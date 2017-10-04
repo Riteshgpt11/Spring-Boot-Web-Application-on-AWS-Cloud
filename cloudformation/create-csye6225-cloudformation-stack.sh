b3a4a245b0fd055c6bf964d2b19b25210c36dba3
Riteshgpt11 <31876546+Riteshgpt11@users.noreply.github.com>
2017-10-04 10:21:17 -0400

    Update create-csye6225-cloudformation-stack.sh
    Changes  to check number of arguments

--- cloudformation/create-csye6225-cloudformation-stack.sh
+++ cloudformation/create-csye6225-cloudformation-stack.sh
@@ -3,4 +3,15 @@
 #Rohan Magare, magare.r@husky.neu.edu, 001231457
 #Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
 #Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
+
+##Check if enough arguements are passed
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: Stack name not provided"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: Too many Arguments"
+  exit 2
+fi
+
+##Creating Stack
 aws cloudformation create-stack --stack-name "$1" --template-body file://create-ec2-stack.yml --parameters file://ec2-parameters.json
