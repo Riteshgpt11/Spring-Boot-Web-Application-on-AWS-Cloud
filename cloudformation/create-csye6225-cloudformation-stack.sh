f76987928e373d88dab6a4f77285f35fb040df6b
Riteshgpt11 <31876546+Riteshgpt11@users.noreply.github.com>
2017-10-04 09:28:52 -0400

    Update create-csye6225-cloudformation-stack.sh

--- cloudformation/create-csye6225-cloudformation-stack.sh
+++ cloudformation/create-csye6225-cloudformation-stack.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+#Yogita Jain, jain.yo@husky.neu.edu, 001643815
+#Rohan Magare, magare.r@husky.neu.edu, 001231457
+#Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
+#Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
 aws cloudformation create-stack --stack-name "$1" --template-body file://create-ec2-stack.yml --parameters file://ec2-parameters.json
