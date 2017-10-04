16329658e1f2e38fe9ec913b0423bcb1783a1935
Riteshgpt11 <31876546+Riteshgpt11@users.noreply.github.com>
2017-10-04 09:32:45 -0400

    Update terminate-ec2-instance.sh

--- script/terminate-ec2-instance.sh
+++ script/terminate-ec2-instance.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Yogita Jain, jain.yo@husky.neu.edu, 001643815
+#Rohan Magare, magare.r@husky.neu.edu, 001231457
+#Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
+#Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
+
 ##Disabling instance termination protection
 aws ec2 modify-instance-attribute --instance-id $1 --no-disable-api-termination
 
