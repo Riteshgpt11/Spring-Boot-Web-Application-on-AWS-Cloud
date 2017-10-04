b61c408d539e1d0cf7a4bcb4d638cf0b990f859e
Riteshgpt11 <31876546+Riteshgpt11@users.noreply.github.com>
2017-10-04 10:23:00 -0400

    Update delete-csye6225-cloudformation-stack.sh
    Change to check arguments

--- cloudformation/delete-csye6225-cloudformation-stack.sh
+++ cloudformation/delete-csye6225-cloudformation-stack.sh
@@ -5,6 +5,15 @@
 #Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
 #Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
 
+##Check if enough arguements are passed
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: Stack name not provided"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: Too many Arguments"
+  exit 2
+fi
+
 # fetch instance id
 instanceid=`aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filters "Name=tag-key,Values=aws:cloudformation:stack-name" "Name=tag-value,Values=$1" --output text`
 echo "$instanceid"
