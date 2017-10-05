a809e34a9cf3e2dd12e88870493ff64f06a050b6
gupta.rite@husky.neu.edu <magare.r@husky.neu.edu>
2017-10-04 22:55:56 -0400

    exception handling

--- script/terminate-ec2-instance.sh
+++ script/terminate-ec2-instance.sh
@@ -5,6 +5,8 @@
 #Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
 #Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
 
+set -e
+
 ##Check if enough arguements are passed
 if [ $# -lt 1 ]; then
   echo 1>&2 "$0: Instance Id not provided"
@@ -19,15 +21,17 @@ aws ec2 modify-instance-attribute --instance-id $1 --no-disable-api-termination
 
 ##Fetching Security Group Id
 sg_id=`aws ec2 describe-instances --instance-ids $1 | grep SECURITYGROUPS | awk '{print $2}'`
-echo $sg_id
+#echo $sg_id
 
 ##Terminating the instance
 aws ec2 terminate-instances --instance-ids $1
 
+echo "Waiting for the instance to get terminated"
+
 ##Waiting for the instance to get terminated
 aws ec2 wait instance-terminated --instance-ids $1
 
-echo "EC2 instance deleted"
+echo "EC2 instance deleted!"
 
 ##Deleting the security group
 aws ec2 delete-security-group --group-id "$sg_id"
