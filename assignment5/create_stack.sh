7f3d8f9d4caeca9da1343c2c18ae1d5726494f94
yogitaj508 <jain.yo@husky.neu.edu>
2017-10-11 11:37:43 -0400

    Cloudformation stack

--- assignment5/create_stack.sh
+++ assignment5/create_stack.sh
@@ -3,7 +3,7 @@
 #Rohan Magare, magare.r@husky.neu.edu, 001231457
 #Pratiksha Shetty, shetty.pr@husky.neu.edu, 001643697
 #Ritesh Gupta, gupta.rite@husky.neu.edu, 001280361
-
+set -e
 ##Check if enough arguements are passed
 if [ $# -lt 1 ]; then
   echo 1>&2 "$0: Stack name not provided"
@@ -13,20 +13,8 @@ elif [ $# -gt 1 ]; then
   exit 2
 fi
 
-#aws cloudformation create-stack --stack-name "$1" --template-body file://merged_stack.yml --parameters file://merged-ec2-parameters.json
-#aws cloudformation create-stack --stack-name "$1" --template-body file://create_security_groups_1.yml --parameters file://ec2-parameters.json
-aws cloudformation create-stack --stack-name "$1" --template-body file://create-RDS-instance.yml --parameters file://testSubnet.json
+aws cloudformation create-stack --stack-name "$1" --template-body file://cloudformation-stack.yml --parameters file://stack-parameters.json
 
+aws cloudformation wait stack-create-complete --stack-name $1
 
-# Create a security group
-#aws cloudformation create-stack --stack-name "$1" --template-body file://create_security_groups.yml --parameters file://ec2-parameters.json
-
-# create a S3 bucket
-#aws cloudformation create-stack --stack-name "$1" --template-body file://create-s3-bucket.yml
-
-#create a Dynamodb
-#aws cloudformation create-stack --stack-name "$1" --template-body file://create-dynamoDBTable.yml --parameters file://DynamoDB-parameters.json
-
-
-# create a RDS
-#aws cloudformation create-stack --stack-name "$1" --template-body file://create-RDS-instance.yml --parameters file://RDS-parameters.json
+echo "stack $1 is created"
