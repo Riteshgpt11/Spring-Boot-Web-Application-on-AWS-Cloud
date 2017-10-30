8e5bdd1b6facc7d7b6f5ceceaaabd1da70ecaa09
gupta.rite@husky.neu.edu <magare.r@husky.neu.edu>
2017-10-30 17:17:03 -0400

    deploying with my bucket name

--- src/main/java/com/csye6225/demo/Service/FileArchiveService.java
+++ src/main/java/com/csye6225/demo/Service/FileArchiveService.java
@@ -28,7 +28,7 @@ import java.time.Instant;
 @Service
 public class FileArchiveService {
 
-    private static final String S3_BUCKET_NAME = "ec2.csye6225-fall2017-guptarite.me.csye6225.com";
+    private static final String S3_BUCKET_NAME = "ec2.csye6225-fall2017-magarer.me.csye6225.com";
     @Autowired
     private AmazonS3 s3Client;
     @Value("${jsa.aws.access_key_id}")
