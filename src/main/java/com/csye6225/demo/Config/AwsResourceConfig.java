9048bdaab4bec9f77ff33632162840b0c1bdab12
rohan magare <magare.r@husky.neu.edu>
2017-11-01 20:09:40 -0400

    aws config updated

--- src/main/java/com/csye6225/demo/Config/AwsResourceConfig.java
+++ src/main/java/com/csye6225/demo/Config/AwsResourceConfig.java
@@ -1,5 +1,2 @@
 package com.csye6225.demo.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
